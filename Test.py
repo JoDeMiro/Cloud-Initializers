44f53fd4215b3c24642bc8d515aae6186eaee960
JoDeMiro <joedemiro@pintye.com>
2022-12-09 19:46:20 +0100

    Test

--- Test.py
+++ Test.py
@@ -11,3 +11,19 @@ print('°˛° ne nézz hülyén úgy sem segít')
 # Start Jupyter
 # jupyter notebook --ip=0.0.0.0 --port=8888 --no-browse
 # jupyter notebook --ip=0.0.0.0 --port=8888 --no-browse &> /dev/null &
+
+print(tf.config.list_physical_devices('GPU'))
+
+print(tf.test.is_gpu_available())
+
+print(tf.test.is_gpu_available(cuda_only=True))
+
+
+from tensorflow.python.client import device_lib
+
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+
+print(get_available_devices())
+
