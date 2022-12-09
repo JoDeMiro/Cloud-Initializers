3b0478dc5da390904515f85f78a9b9e2e2002600
JoDeMiro <joedemiro@pintye.com>
2022-12-09 14:16:29 +0100

    Test

---
+++ Test.py
@@ -0,0 +1,9 @@
+'''Joe De Miro oficial Nespresso advocate and associated partner °˛°'''
+
+import numpy as np
+print(np.__version__)
+
+import tensorflow as tf
+print(tf.__version__)
+
+print('°˛° ne nézz hülyén úgy sem segít')
